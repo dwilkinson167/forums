ea9d993fa0463d8b3bb3479275d28ac73700eb8d
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-06-13 00:17:06 -0700

    Post thread form and exception handling

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ Route::get('/', function () {
 
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
-
 Route::resource('threads', 'ThreadsController');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
