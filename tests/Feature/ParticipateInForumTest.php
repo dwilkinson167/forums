95bc4f648de436197cfe33dc6250bf8531687fb7
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-06-12 22:26:31 -0700

    Added test helpers

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -8,30 +8,30 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ParticipateInForumTest extends TestCase
 {
-	use DatabaseMigrations;
+    use DatabaseMigrations;
 
-	/** @test */
-	function unauthenticated_users_may_not_add_replies()
-	{
-		$this->expectException('Illuminate\Auth\AuthenticationException');
+    /** @test */
+    function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
 
-		$this->post('/threads/1/replies', []);
-	}
-
-
-	/** @test */
-	function an_authenticated_user_may_participate_in_forum_threads()
-	{
-
-		$this->be($user = factory('App\User')->create());
-
-		$thread = factory('App\Thread')->create();
-
-		$reply = factory('App\Reply')->make();
-		$this->post($thread->path() . '/replies', $reply->toArray());
+        $this->post('/threads/1/replies', []);
+    }
 
 
-		$this->get($thread->path())
-			->assertSee($reply->body);
-	}
+    /** @test */
+    function an_authenticated_user_may_participate_in_forum_threads()
+    {
+
+        $this->be($user = factory('App\User')->create());
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply');
+
+        $this->post($thread->path() . '/replies', $reply->toArray());
+
+
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
 }
